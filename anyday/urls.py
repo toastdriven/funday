21a9689a8116a0d6adcb35b52257e1f33ff57d2d
Daniel Lindsley <daniel@toastdriven.com>
2012-09-14 20:53:16 -0500

    Added a URL/view for race-specific options.

--- anyday/urls.py
+++ anyday/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-from core.views import RandomFundayView
+from core.views import RandomFundayView, RaceRandomFundayView
 
 
 admin.autodiscover()
@@ -12,4 +12,7 @@ urlpatterns = patterns(
         RandomFundayView.as_view(template_name='home.html'),
         name='home'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^(?P<race>\w+)/$',
+        RaceRandomFundayView.as_view(),
+        name='random_race'),
 )
